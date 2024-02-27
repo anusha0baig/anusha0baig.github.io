8acce66b5e7702d4ca916dd5b517cf6c8e1f858d
anusha0baig <anusha.baig@torontomu.ca>
2024-02-26 23:39:00 -0500

    commit
    make changes 2

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert(“Hello World”!)
+function promptForName() {
+    let name = prompt("Please enter your name:");
+    document.getElementById("outputName").innerText = "Hello, " + name + "!";
+}
+
+function enlargeImage() {
+    document.getElementById("Snapchat-1805152574").style.display = "none";
+    document.getElementById("enlargedImage").style.display = "inline";
+}
+
+function shrinkImage() {
+    document.getElementById("Snapchat-1805152574").style.display = "inline";
+    document.getElementById("enlargedImage").style.display = "none";
+}
+
+function highlightNavItem(navItem) {
+    navItem.style.color = "blue";
+}
+
+function unhighlightNavItem(navItem) {
+    navItem.style.color = "black";
+}
